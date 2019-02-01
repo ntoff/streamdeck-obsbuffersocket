b66d30bd9039a535df6eedd563c350c99a776a93
ntoff <norahtihtzoff@gmail.com>
2019-02-02 02:24:36 +1000

    switch to using button states
    use button states to display status rather than single set image

--- Sources/com.ntoff.obsbuffersocket.sdPlugin/app.js
+++ Sources/com.ntoff.obsbuffersocket.sdPlugin/app.js
@@ -9,166 +9,244 @@ Without this feature, when switching pages in this plugin, the replay buffer but
 
 Some of this code was lifted from the Elgato analog clock example.
 */
-
-var DestinationEnum = Object.freeze({
+var obsHost = "localhost"
+    obsWebsocket = null,
+    obsIsLaunched = 0,
+    DestinationEnum = Object.freeze({
     HARDWARE_AND_SOFTWARE: 0,
     HARDWARE_ONLY: 1,
     SOFTWARE_ONLY: 2
-});
+    });
+
 $SD.on('connected', conn => connected(conn));
 
 function connected (jsn) {
-    debugLog('Connected Plugin:', jsn);
     
     $SD.on('applicationDidLaunch', jsonObj =>
-        //connectOBS() 
-        
-        /* connecting immediately seems to crash obs, not sure if it's trying to connect too soon after launching, or just because
-        * "reasons" (custom built obs-websocket plugin with added functionality this plugin relies on).
-        * For now all it does is show something in the console log.
-        */
-        
-        console.log("OBS has been launched, praise the sun!")
+        this.obsLaunched(jsonObj)
     );
-    //shared features
-    $SD.on('com.ntoff.obsbuffersocket.toggle.keyDown', jsonObj =>
-        sharedButtons.onKeyDown(jsonObj)
-    );
-    $SD.on('com.ntoff.obsbuffersocket.save.keyDown', jsonObj =>
-        sharedButtons.onKeyDown(jsonObj)
+    $SD.on('applicationDidTerminate', jsonObj =>
+        this.obsLaunched(jsonObj)
     );
     //toggle buffer feature
-    $SD.on('com.ntoff.obsbuffersocket.toggle.willAppear', jsonObj =>
+    $SD.on('com.obsbuffersocket.toggle.willAppear', jsonObj =>
         toggleBuffer.onToggleAppear(jsonObj)
     );
-    $SD.on('com.ntoff.obsbuffersocket.toggle.keyUp', jsonObj =>
+    $SD.on('com.obsbuffersocket.toggle.keyDown', jsonObj =>
+        toggleBuffer.onToggleKeyDown(jsonObj)
+    );
+    $SD.on('com.obsbuffersocket.toggle.keyUp', jsonObj =>
         toggleBuffer.onToggleKeyUp(jsonObj)
     );
+    $SD.on('com.obsbuffersocket.toggle.willDisappear', jsonObj =>
+        toggleBuffer.onWillDisappear(jsonObj)
+    );
     //save buffer feature
-    $SD.on('com.ntoff.obsbuffersocket.save.willAppear', jsonObj =>
+    $SD.on('com.obsbuffersocket.save.willAppear', jsonObj =>
         saveBuffer.onSaveAppear(jsonObj)
     );
-    $SD.on('com.ntoff.obsbuffersocket.save.keyUp', jsonObj =>
+    $SD.on('com.obsbuffersocket.save.keyDown', jsonObj =>
+        saveBuffer.onSaveKeyDown(jsonObj)
+    );
+    $SD.on('com.obsbuffersocket.save.keyUp', jsonObj =>
         saveBuffer.onSaveKeyUp(jsonObj)
+    );
+    $SD.on('com.obsbuffersocket.save.willDisappear', jsonObj =>
+        saveBuffer.onWillDisappear(jsonObj)
     );
 }
 
-var sharedButtons = {
-    //type: 'com.ntoff.obsbuffersocket.sharedButtons',
-    cache: {},
-    
-    onKeyDown: function (jsn) {
-        connectOBS();
-    }
-};
-
 var toggleBuffer = {
-    type: 'com.ntoff.obsbuffersocket.toggle',
+    type: 'com.obsbuffersocket.toggle',
     cache: {},
-
-    onToggleAppear: function (jsn) {
-        buttonHandler(jsn, "GetReplayBufferStatus");
+    getContextFromCache: function (ctx) {
+        return this.cache[ctx];
     },
-
-    onToggleKeyUp: function (jsn) {
-        buttonHandler(jsn, "StartStopReplayBuffer");
-    }
+    onToggleAppear: function (toggleJson) {
+        if (!obsWebsocket) {
+            SDApi.send(toggleJson.context, 'showAlert', {});
+            return;
+        }
+        else {
+            getReplayStatus(toggleJson);
+        }
+    },
+    onToggleKeyDown: function (toggleJson) {
+        if (obsWebsocket) {
+            if (toggleJson.payload.state == 1) {
+                obsWebsocket.send(JSON.stringify({"message-id": "2", "request-type": "StopReplayBuffer"}));
+            }
+            else if (toggleJson.payload.state == 0) {
+                obsWebsocket.send(JSON.stringify({"message-id": "2", "request-type": "StartReplayBuffer"}));
+            }
+            getReplayStatus(toggleJson); 
+        }
+        else { 
+            SDApi.send(toggleJson.context, 'showAlert', {});
+        }
+    },
+    onToggleKeyUp: function (toggleJson) {
+        getReplayStatus(toggleJson);
+    },
+    onWillDisappear: function (toggleJson) {
+        let found = this.getContextFromCache(toggleJson.context);
+        if (found) {
+            delete this.cache[toggleJson.context];
+        }
+    },
 };
 
 var saveBuffer = {
-    type: 'com.ntoff.obsbuffersocket.save',
+    type: 'com.obsbuffersocket.save',
     cache: {},
-    
-    onSaveAppear: function (jsn) {
+    getContextFromCache: function (ctx) {
+        return this.cache[ctx];
+    },
+    onSaveAppear: function (saveJson) {
         if (!obsWebsocket) {
-            SDApi.send(jsn.context, 'showAlert', {});
+            SDApi.send(saveJson.context, 'showAlert', {});
             return;
         }
     },
-    
-    onSaveKeyUp: function (jsn) {
-        buttonHandler(jsn, "SaveReplayBuffer");
-    }
+    onSaveKeyDown: function (saveJson) {
+        if (obsIsLaunched) {
+            connectOBS();
+        }
+        else {
+            console.log("Can't connect, OBS doesn't appear to be running.");
+        }
+    },
+    onSaveKeyUp: function (saveJson) {
+        if (!obsWebsocket) {
+            SDApi.send(saveJson.context, 'showAlert', {});
+            return;
+        }
+        try{
+            obsWebsocket.send(JSON.stringify({"message-id": "3", "request-type": "SaveReplayBuffer"}));
 
+            obsWebsocket.onmessage = function (evt) { 
+                var jsonObj = JSON.parse(evt.data);
+                if (jsonObj.hasOwnProperty('status')) {
+                    status = jsonObj['status'];
+                    msgid = jsonObj['message-id'];
+                    if (status == "ok" && msgid == "3") {
+                        SDApi.send(saveJson.context, 'showOk', {});
+                        return;
+                    }
+                    if (status == "error" && msgid == "3") {
+                        SDApi.send(saveJson.context, 'showAlert', {});
+                        console.log(jsonObj);
+                        return;
+                    }
+                }
+            }
+        }
+        catch(error) {
+            SDApi.send(saveJson.context, 'showAlert', {});
+        }
+    },
+    onWillDisappear: function (saveJson) {
+        let found = this.getContextFromCache(saveJson.context);
+        if (found) {
+            delete this.cache[saveJson.context];
+        }
+    },
 };
 
-function loadImageAsDataUri(url, callback) {
-    var image = new Image();
-
-    image.onload = function () {
-        var canvas = document.createElement("canvas");
-
-        canvas.width = this.naturalWidth;
-        canvas.height = this.naturalHeight;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
-        callback(canvas.toDataURL("image/png"));
-    };
-
-    image.src = url;
-};
-
-function buttonHandler(jsn, controlType) {
-    var controlType = controlType;
-
+function getReplayStatus(toggleJson) {
     if (!obsWebsocket) {
-        SDApi.send(jsn.context, 'showAlert', {});
+        SDApi.send(toggleJson.context, 'setState', {
+            payload: {
+                "state": 0
+            }
+        });
+        SDApi.send(toggleJson.context, 'showAlert', {});
         return;
     }
-
     try{
-        obsWebsocket.send(JSON.stringify({"message-id": "2", "request-type": controlType}));
+        obsWebsocket.send(JSON.stringify({"message-id": "2", "request-type": "GetReplayBufferStatus"}));
     }
     catch(error) {
-        SDApi.send(jsn.context, 'showAlert', {});
     }
-
+    
     obsWebsocket.onmessage = function (evt) { 
-
-        var jsonObj = JSON.parse(evt.data),
-            imageName = "",
-            replayActive = "";
-            var status = "";
-
+        var jsonObj = JSON.parse(evt.data);
+        //console.log(jsonObj);
         if (jsonObj.hasOwnProperty('replay-active') || jsonObj.hasOwnProperty('update-type')) {
-            replayActive = jsonObj['replay-active'] || jsonObj['update-type'];
+            replayActive = jsonObj['replay-active'] || false;
+            updateType =  jsonObj['update-type'];
+            if (replayActive == true || updateType == "ReplayStarted") {
+                SDApi.send(toggleJson.context, 'setState', {
+                    payload: {
+                        "state": 1
+                    }
+                });
+                return;
+            }
+            else if (!replayActive || replayActive == false || updateType == "ReplayStopped") {
+                SDApi.send(toggleJson.context, 'setState', {
+                    payload: {
+                        "state": 0
+                    }
+                });
+                return;
+            }
         }
         else if (jsonObj.hasOwnProperty('status')) {
             status = jsonObj['status'];
+            msgid = jsonObj['message-id'];
+            if (status == "error" && msgid == "2") {
+                SDApi.send(toggleJson.context, 'setState', {
+                    payload: {
+                        "state": 0
+                    }
+                });
+                SDApi.send(toggleJson.context, 'showAlert', {});
+                console.log(JSON.parse(evt.data)); //spit out the error in the console
+                return;
+            }
         }
-        else {
-            return;
-        }
-        
-        if (status == "ok") {
-            SDApi.send(jsn.context, 'showOk', {});
-            return;
-        }
-        else if (status == "error") {
-            SDApi.send(jsn.context, 'showAlert', {});
-            console.log(JSON.parse(evt.data)); //spit out an error in the console
-            return;
-        }
-        
-        if (replayActive == true || replayActive == "ReplayStarted") {
-            imageName = "obsbufferon"
-        }
-        else if (replayActive == false || replayActive == "ReplayStopped") {
-            imageName = "obsbuffer"
-        }
-        else {
-            return;
-        }
+    }
+};
 
-        loadImageAsDataUri(`${imageName}.png`, function (imgUrl) {
-
-            SDApi.send(jsn.context, 'setImage', {
-                payload: {
-                    image: imgUrl || "",
-                    target: DestinationEnum.HARDWARE_AND_SOFTWARE
-                }
-            });
-        });
+function connectOBS() {
+    //if there's no connection, create one
+    if (!obsWebsocket){
+        try{
+            obsWebsocket = new WebSocket("ws://"+obsHost+":4444");
+        }
+        catch(error){}
+    }
+    obsWebsocket.onopen = function() {
+        obsWebsocket.send(JSON.stringify({"message-id": "1", "request-type": "GetAuthRequired"}));
+        console.log("Socket Opened");
     };
+    obsWebsocket.onerror = function(event) {
+        console.log("Socket Error")
+        obsWebsocket = null;
+    }
+    //in case we can't connect and the socket is closed
+    obsWebsocket.onclose = function() { 
+        console.log("Socket Closed")
+        obsWebsocket = null; //nullify the socket if obs is closed
+    };
+};
+
+//not currently used
+function disconnectOBS() {
+    obsWebsocket.onclose = function() { 
+        obsWebsocket = null; //nullify the socket if obs is closed
+    };
+};
+
+function obsLaunched(jsonObj){
+    var event = jsonObj.event;
+    if (event == "applicationDidLaunch") {
+        console.log("OBS has been started");
+        obsIsLaunched = 1;
+    }
+    else if (event == "applicationDidTerminate") {
+        console.log("OBS has been terminated")
+        obsIsLaunched = 0;
+    }
 };
